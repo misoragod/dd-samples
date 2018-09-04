13f19d12bcded329dd021937acf18774b4ac8394
misoragod <miyamoto@think-next.tech>
2018-09-04 17:06:22 +0900

    bind queue trigger and Blob

--- Azure/UploadPlan/js/main.js
+++ Azure/UploadPlan/js/main.js
@@ -1,17 +1,26 @@
 var blobUri;
 var CONTAINER_NAME;
 var SAS_Token;
+var QUEUE_NAME;
+
 $("#send").click(function(){        
     // List container name  
     $.getJSON("./config.json", function(data) {
-        blobUri = data.host;
+        queueUri = data.queueUri;
+        QUEUE_NAME = data.queueName;
+        blobUri = data.blobUri;
         SAS_Token = data.SASKey;
         CONTAINER_NAME = data.container;
     })
+    console.log(queueUri);
+    console.log(QUEUE_NAME);
     console.log(blobUri);
     console.log(SAS_Token);
     console.log(CONTAINER_NAME);
-　　
+　　//create Azure storage queue object
+    var queueService = AzureStorage.Queue.createQueueServiceWithSas(queueUri, SAS_Token);
+ 
+    //Create Azure storage blob object
     var blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, SAS_Token);
 
     blobService.listContainersSegmented(null, function (error, results) {
@@ -23,7 +32,8 @@ $("#send").click(function(){
                 console.log(results.entries[i].name)
             }
         }
-    })
+    }) 
+    
     console.log($("#fileinput").prop('files')[0]);
  
        
@@ -40,6 +50,35 @@ $("#send").click(function(){
             console.log("upload blob failed") 
         } else {
             console.log("upload blob successfully")
+        }
+    })
+    // Send file name to Queue
+    var MESSAGE = file.name; 
+    queueService.listQueuesSegmented(null, function (error, results) {
+        if (error) {
+        // List queue error
+        } else {
+            for (var i = 0, queue; queue = results.entries[i]; i++) {
+                console.log(queue.name);
+            }
+        }
+    })
+    // Encode message to Base64
+    var encoder = new AzureStorage.Queue.QueueMessageEncoder.TextBase64QueueMessageEncoder();
+    console.log("QueueNam: " + QUEUE_NAME); 
+    console.log("Message : " + MESSAGE); 
+    queueService.createMessage(QUEUE_NAME, encoder.encode(MESSAGE), function (error, results, response) {
+        if (error) {
+            // Create message error
+        } else {
+        // Create message successfully
+        $("#msg").text("Sent blob to Azure");
+        $("#msg").fadeIn().queue(function() {
+            setTimeout(function(){$("#msg").dequeue();
+            }, 3000);
+        });
+         $("#msg").fadeOut();
+        console.log("Sent blob to Azure")
         }
     })
 //    refreshProgress();
